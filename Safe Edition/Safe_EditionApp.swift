70a18b82b140ab074da2e344dbbff60e4e142409
kheladzedev <kheladzedev@users.noreply.github.com>
2025-10-21 08:18:54 -0700

    Initial Commit

---
+++ Safe Edition/Safe_EditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Safe_EditionApp.swift
+//  Safe Edition
+//
+//  Created by Edward on 21.10.25.
+//
+
+import SwiftUI
+
+@main
+struct Safe_EditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
